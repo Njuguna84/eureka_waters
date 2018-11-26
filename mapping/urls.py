8dc65b8027a2df4e459f85e39cf589d487fa76e7
Njuguna84 <dennix.njuguna85@gmail.com>
2018-11-26 15:14:54 +0300

    add disconn clients, and multiquery

--- mapping/urls.py
+++ mapping/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import client_meters, k_roads
+from .views import k_roads, client_meters, disconneted_clients, multi_query
 
 app_name='eureka_maps'
 
 urlpatterns = [
     path('kiserian_roads_data/', k_roads, name='roads'),
     path('client_meters/', client_meters, name='c_meters'),
-
+    path('disconnected_clients', disconneted_clients, name='disconnected'),
+    path('multi_query', multi_query, name="multi"),
 ]
